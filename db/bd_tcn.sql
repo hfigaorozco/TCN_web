c9834f116021aeff769350f3023156dec89080c4
Hector <hfigaorozco@gmail.com>
2026-03-01 21:43:45 -0800

    Mejoras en la UI de login y registro

--- db/bd_tcn.sql
+++ db/bd_tcn.sql
@@ -4,13 +4,13 @@ USE tcn;
 -- 0. TABLA USUARIO
 CREATE TABLE usuario(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email varchar(10) not null unique,
+    email varchar(40) not null unique,
     password varchar(16) not null,
     rol varchar(15) not null
 ); 
 
--- 1. TABLA ESTADO
-CREATE TABLE estado (
+-- 1. TABLA CIUDAD
+CREATE TABLE ciudad (
     codigo VARCHAR(3) PRIMARY KEY,
     nombre VARCHAR(30) NOT NULL UNIQUE
 );
@@ -68,21 +68,14 @@ CREATE TABLE pasajero (
     telefono VARCHAR(10) NOT NULL
 );
 
--- 9. TABLA CIUDAD
-CREATE TABLE ciudad (
-    codigo VARCHAR(3) PRIMARY KEY,
-    nombre VARCHAR(30) NOT NULL UNIQUE,
-    estado VARCHAR(3) NOT NULL
-);
-
--- 10. TABLA MODELO
+-- 9. TABLA MODELO
 CREATE TABLE modelo (
     clave VARCHAR(5) PRIMARY KEY,
     nombre VARCHAR(15) NOT NULL,
     marca VARCHAR(5) NOT NULL
 );
 
--- 11. TABLA RUTA
+-- 10. TABLA RUTA
 CREATE TABLE ruta (
     codigo VARCHAR(9) PRIMARY KEY,
     distancia FLOAT NOT NULL,       
@@ -90,7 +83,7 @@ CREATE TABLE ruta (
     ciudadDestino VARCHAR(3) NOT NULL       
 );
 
--- 12. TABLA AUTOBUS
+-- 11. TABLA AUTOBUS
 CREATE TABLE autobus(
     numero INT PRIMARY KEY,
     matricula CHAR(6) NOT NULL UNIQUE,
@@ -102,7 +95,7 @@ CREATE TABLE autobus(
     modelo VARCHAR(5) NOT NULL
 );
 
--- 13. TABLA CORRIDA
+-- 12. TABLA CORRIDA
 CREATE TABLE corrida(
     numero INT PRIMARY KEY AUTO_INCREMENT, 
     hora_salida TIME NOT NULL, 
@@ -117,7 +110,7 @@ CREATE TABLE corrida(
     estado VARCHAR(3) NOT NULL 
 );
 
--- 14. TABLA RESERVACION
+-- 13. TABLA RESERVACION
 CREATE TABLE reservacion (
     numero INT PRIMARY KEY AUTO_INCREMENT, 
     fecha DATE NOT NULL, 
@@ -131,7 +124,7 @@ CREATE TABLE reservacion (
     usuario INT NULL
 );
 
--- 15. TABLA ASIENTO
+-- 14. TABLA ASIENTO
 CREATE TABLE asiento(
     clave VARCHAR(6) PRIMARY KEY,
     numero INT NOT NULL,
@@ -139,14 +132,14 @@ CREATE TABLE asiento(
     autobus INT NOT NULL
 );
 
--- 16. TABLA ASIENTO_RESERVACION
+-- 15. TABLA ASIENTO_RESERVACION
 CREATE TABLE asiento_reservacion(
     asiento VARCHAR(6) NOT NULL,
     reservacion INT NOT NULL,
     PRIMARY KEY (asiento, reservacion)
 );
 
--- 17. TABLA BOLETO
+-- 16. TABLA BOLETO
 CREATE TABLE boleto(
     numero INT AUTO_INCREMENT PRIMARY KEY,
     precio FLOAT NOT NULL,
@@ -156,7 +149,7 @@ CREATE TABLE boleto(
     corrida INT NOT NULL
 );
 
--- 18. TABLA CORRIDA_ASIENTO
+-- 17. TABLA CORRIDA_ASIENTO
 CREATE TABLE corrida_asiento(
     corrida INT NOT NULL,
     asiento VARCHAR(6) NOT NULL,
@@ -170,12 +163,6 @@ CREATE TABLE corrida_asiento(
 ALTER TABLE modelo
 ADD CONSTRAINT fk_modelo_marca
 FOREIGN KEY (marca) REFERENCES marca(clave)
-ON UPDATE CASCADE ON DELETE RESTRICT;
-
--- CIUDAD
-ALTER TABLE ciudad
-ADD CONSTRAINT fk_ciudad_estado
-FOREIGN KEY (estado) REFERENCES estado(codigo)
 ON UPDATE CASCADE ON DELETE RESTRICT;
 
 -- RUTA
