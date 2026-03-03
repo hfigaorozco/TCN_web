f22a9f323fdb43036b2395e9d07be30eb3ed2c26
Hector <hfigaorozco@gmail.com>
2026-03-02 23:38:47 -0800

    Terminando navegacion y ultimos dialogos

--- statics/js/dashboard.js
+++ statics/js/dashboard.js
@@ -2,8 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const btnUsuario = document.getElementById('btn-usuario');
     const btnModificarContra = document.getElementById('btn-cambiar-contra');
+    const btnVenderBoletos = document.getElementById('btn-venderBoletos');
+
     const dialogoUsuario = document.getElementById('dialogo-perfil');
     const dialogoContra = document.getElementById('dialogo-contrasena');
+    const dialogoBusqueda = document.getElementById('dialogo-buscar');
 
 
     // Abrir diálogo de perfil
@@ -15,6 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
     btnModificarContra.addEventListener('click', () => {
         dialogoContra.showModal();
     });
-    
+
+    // Abrir diálogo de búsqueda de viaje
+    btnVenderBoletos.addEventListener('click', () => {
+        dialogoBusqueda.showModal();
+    });
+
 
 });
