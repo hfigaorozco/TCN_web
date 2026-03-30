2629ddbe461d7b8988c2f8ab000cfea2952a6a85
GaliDaVinci <leonardovaldez583@gmail.com>
2026-03-30 13:56:59 -0700

    > Registro y muestra de operadores

--- apps/operadores/views.py
+++ apps/operadores/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Operador
 
 # Create your views here.
 
 def pagina_operadores(request):
-    return render(request, 'operadores.html')
+    
+    if request.method == 'POST' and 'action' in request.POST:
+        
+        if request.POST['action'] == 'agregar_operador':
+            
+            Operador.objects.create(
+                nombre = request.POST['nombre'],
+                apellPat = request.POST['apellPat'],
+                apellMat = request.POST['apellMat'],
+                fechaNac = request.POST['fechaNac'],
+                telefono = request.POST['telefono'],
+                fechaContrato = request.POST['fechaContrato'],
+            )
+            return redirect('operadores')
+    
+    operadores = Operador.objects.all()
+    
+    return render(request, 'operadores.html', {'operadores': operadores})
+
