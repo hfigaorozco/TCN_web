cfc0fbcfb4f460423911da159b7baf0466f9f807
Hector <hfigaorozco@gmail.com>
2026-04-01 18:59:06 -0700

    Juntando archivos del dashboard

--- apps/pasajeros/models.py
+++ apps/pasajeros/models.py
@@ -18,3 +18,19 @@ class TipoPasajero(models.Model):
     
     def __str__(self):
         return f"{self.descripcion} ({self.porcentaje_desc}%)"
+    
+
+class Pasajero(models.Model):
+    numero = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    apellPat = models.CharField(max_length=20)
+    apellMat = models.CharField(max_length=20, null=True, blank=True)
+    edad = models.IntegerField()
+    correoElect = models.EmailField(max_length=40, null=True, blank=True)
+    telefono = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'pasajero' 
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellPat}"
