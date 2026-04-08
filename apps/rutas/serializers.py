591fe4d5721ccaa370d124e78fbcf97b674c305d
Tu Nombre <juanmanuellunatico3467@gmail.com>
2026-04-07 18:48:13 -0700

    api y seriazlier de asientos y ciudades

---
+++ apps/rutas/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Ciudad
+
+class CiudadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ciudad
+        fields = ['codigo', 'nombre']
